aeeed475b013854cff8b29516aab98a84550f0e3
PRakhar <srivastavaprakhar3010@gmail.com>
2024-04-03 01:19:36 +0530

    temporary check

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,25 @@ import { LampContainer } from "./components/ui/lamp";
 import { FloatingNav } from "../src/components/ui/floating-navbar";
 import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
 import WalletIcon from "/public/wallet-svgrepo-com.svg";
-import Spline from '@splinetool/react-spline';
+import Spline from "@splinetool/react-spline";
 
 function LampDemo() {
   return (
     <div className="relative">
       <LampContainer>
         <h1 className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
-          Mercado<br /><h2 className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-2xl font-medium tracking-tight text-transparent md:text-2xl">Own the Future. Mercado NFTs</h2>
+          Mercado
+          <br />
+          <h2 className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-2xl font-medium tracking-tight text-transparent md:text-2xl">
+            Own the Future. Mercado NFTs
+          </h2>
         </h1>
       </LampContainer>
-      <img src="/public/logoHere2.png" alt="Logo" className="absolute w-68 h-56 right-2 top-32 " />
+      <img
+        src="/public/logoHere2.png"
+        alt="Logo"
+        className="absolute w-96 h-60 right-2 top-32 "
+      />
       <Spline
         className="absolute top-0 right-0 bottom-0 left-0 z-10"
         scene="https://prod.spline.design/tHRctOE2M1c4bO6u/scene.splinecode"
@@ -37,19 +45,27 @@ function App() {
     {
       name: "Contact",
       link: "/contact",
-      icon: <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />,
+      icon: (
+        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
     },
     {
       name: "ConnectWallet",
       link: "/wallet",
-      icon: <img src={WalletIcon} alt="Wallet" className="h-4 w-4 text-neutral-500 dark:text-white" />,
+      icon: (
+        <img
+          src={WalletIcon}
+          alt="Wallet"
+          className="h-4 w-4 text-neutral-500 dark:text-white"
+        />
+      ),
     },
   ];
 
   return (
     <div>
       <FloatingNav navItems={navItems} />
-      
+
       <LampDemo />
     </div>
   );
