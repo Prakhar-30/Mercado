c11d6d3f081b22f2ab1c44aef1274e59cf6520d2
PRakhar <srivastavaprakhar3010@gmail.com>
2024-04-03 01:09:32 +0530

    added logo file in public and displayed it in webpage temprarily

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function LampDemo() {
           Mercado<br /><h2 className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-2xl font-medium tracking-tight text-transparent md:text-2xl">Own the Future. Mercado NFTs</h2>
         </h1>
       </LampContainer>
+      <img src="/public/logoHere2.png" alt="Logo" className="absolute w-68 h-56 right-2 top-32 " />
       <Spline
         className="absolute top-0 right-0 bottom-0 left-0 z-10"
         scene="https://prod.spline.design/tHRctOE2M1c4bO6u/scene.splinecode"
