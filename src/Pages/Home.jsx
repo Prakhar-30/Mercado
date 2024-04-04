d9462358254c4784d6c9b565953a3ccc586ab3cf
PRakhar <srivastavaprakhar3010@gmail.com>
2024-04-04 13:47:08 +0530

    Modified home page added artists section logo, creators section logo, added components BackgroundGradientDemo, BackgroundGradient, Animated divs.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import { Navbar } from "../components/Navbar";
 import { cn } from "../utils/cn";
 import GlowingButton from "../components/GlowingButton";
+import AnimatedText from "../components/AnimatedDiv";
+import { BackgroundGradientDemo } from "../components/BackgroundGradientDemo";
 
 export function Home() {
   return (
     <div
       className={cn(
-        "relative flex min-h-screen flex-col items-center justify-center overflow-auto bg-slate-950 w-full"
+        "relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-slate-950 w-full"
       )}
     >
       <Navbar />
 
-      <div className="!flex justify-between mt-56">
+      <div className="flex justify-between mt-56">
         <div className="text-slate-400 text-7xl p-8 ml-24 mr-60">
           Welcome to Mercado, where the future is yours to own.
         </div>
@@ -33,6 +35,68 @@ export function Home() {
       <div class="text-slate-400 text-6xl text-center mt-10 border border-slate-600 rounded-3xl pt-32 pb-32 mb-6 ml-1 mr-1">
         Unlock the potential of digital ownership with Mercado
       </div>
+
+      <div className="Details">
+        <div className=" text-slate-400 text-8xl mt-20 min-w-full  ml-8">
+          Artists
+        </div>
+        <div className="flex mt-11">
+          <div className="ml-4 mt-6 text-center w-3/5">
+            <AnimatedText>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Eligendi, quo! Dolore distinctio error consequuntur, accusamus qui
+              quis nulla reprehenderit. Quos, veniam at ipsum labore expedita
+              fugit dolor debitis dolorum dolores?
+            </AnimatedText>
+          </div>
+          <div className="text-center w-2/5 pl-8">
+            <BackgroundGradientDemo>
+              <div className="border rounded-lg">
+                <img 
+                className="size-80"
+                src="/public/ArtistImage.jpeg" alt="logo" />
+              </div>
+            </BackgroundGradientDemo>
+          </div>
+        </div>
+        <div className="ml-4">
+          <GlowingButton text="Enter as Artist" />
+        </div>
+      </div>
+
+      <div className="Details">
+  <div className="flex justify-end">
+    <div className="text-slate-400 text-8xl mt-20 min-w-full text-right mr-8">
+      Creators
+    </div>
+  </div>
+
+  <div className="flex mt-11">
+    <div className="text-center w-2/5 pr-8">
+      <BackgroundGradientDemo>
+        <div className="border rounded-lg">
+          <img 
+          className="size-80"
+          src="/public/Creators.jpg" alt="logo" />
+        </div>
+      </BackgroundGradientDemo>
+    </div>
+    <div className="mr-4 mt-6 text-center w-3/5">
+      <AnimatedText>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi,
+        quo! Dolore distinctio error consequuntur, accusamus qui quis nulla
+        reprehenderit. Quos, veniam at ipsum labore expedita fugit dolor
+        debitis dolorum dolores?
+      </AnimatedText>
+    </div>
+  </div>
+  <div className="flex justify-end mt-0">
+    <div className="mr-4 mt-0">
+      <GlowingButton text="Enter as Creator" />
+    </div>
+  </div>
+</div>
+
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ function addVariablesForColors({ addBase, theme }) {
 }
 
 module.exports = {
+  
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
