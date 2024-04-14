874cfffcfb1b78d32c0e8f3d5e7ed849c3a370af
PRakhar <srivastavaprakhar3010@gmail.com>
2024-04-15 01:54:09 +0530

    modified CreateNftPAge amd FileUpload component changes in package-lock.json web3Helpers.js

--- src/contexts/index.jsx
+++ src/contexts/index.jsx
@@ -1,27 +1,20 @@
-import React, { useContext, createContext, useState } from 'react';
-import Web3 from 'web3';
+import React, { useContext, createContext, useState } from "react";
+import Web3 from "web3";
 
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  
-  const [ERC1155_CONTRACT,setERC1155_CONTRACT]=useState(null);
-  const [MercatContract,setMercatContract]=useState(null);
-  const [account,setAccount]=useState(null);
+  const [ERC1155_CONTRACT, setERC1155_CONTRACT] = useState(null);
+  const [MercatContract, setMercatContract] = useState(null);
+  const [account, setAccount] = useState(null);
   // const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
-  
-  
-  
 
-
-  
-
-  
-
+  console.log(ERC1155_CONTRACT);
+  console.log(MercatContract);
 
   return (
     <StateContext.Provider
-      value={{ 
+      value={{
         ERC1155_CONTRACT,
         setERC1155_CONTRACT,
         MercatContract,
@@ -32,7 +25,7 @@ export const StateContextProvider = ({ children }) => {
     >
       {children}
     </StateContext.Provider>
-  )
-}
+  );
+};
 
 export const useStateContext = () => useContext(StateContext);
